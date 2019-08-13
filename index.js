d046069197e7a68cf4d445015c1b6b002eeb86ed
Gabi <gabicasas@gmail.com>
2019-08-13 20:43:29 +0200

    Ejecucion importando fichero

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-'use strict';
-const vm = require('vm');
-const fs = require('fs');
+"use strict";
+const vm = require("vm");
+
+const execFile = require("child_process").execFile;
+var spawn = require("child_process").spawn;
+const fs = require("fs");
 
 /*const code = `
 (function(require) {
@@ -15,58 +18,61 @@ const fs = require('fs');
 })`;
 
 vm.runInThisContext(code)(require);*/
-const http = require('http');
-var url = require('url');
-var events = require('events');
-var eventEmitter = new events.EventEmitter(); 
+const http = require("http");
+var url = require("url");
+var events = require("events");
+var eventEmitter = new events.EventEmitter();
 
 const PUPPETEER_OPTS = {
-    headless: false,
-    slowMo: { default: 300, click: 200, keyup: 10 },
-    devtools: true,
-}
+  headless: false,
+  slowMo: { default: 300, click: 200, keyup: 10 },
+  devtools: true
+};
 
-let paramToSaveData={};
+let paramToSaveData = {};
 
-http.createServer((req, resp) => {
+http
+  .createServer((req, resp) => {
     var method = req.method;
 
     // If post.
-    if("POST" == method)
-    {
-        var postData = '';
+    if ("POST" == method) {
+      var postData = "";
 
-        // Get all post data when receive data event.
-        req.on('data',  (chunk)=>{
+      // Get all post data when receive data event.
+      req.on("data", chunk => {
+        postData += chunk;
+      });
 
-            postData += chunk;
+      req.on("end", () => {
+        console.log("Client post data : " + postData);
 
+        // Parse the post data and get client sent username and password.
+        var postDataObject = JSON.parse(postData);
+        let filename = postDataObject.filename;
+        let code = postDataObject.code;
+        let functionName=postDataObject.functionName;
+
+        // vm.runInThisContext('(function(require,paramToSaveData,eventEmitter) {const PUPPETEER_OPTS='+JSON.stringify(PUPPETEER_OPTS)+'; '+code+''+postDataObject.functionName+'(paramToSaveData,eventEmitter)}); ')(require,paramToSaveData);
+
+        resp.end("++" + JSON.stringify(postDataObject) + "++");
+        fs.writeFile(filename, code, function(err) {
+          if (err) {
+            return console.log(err);
+          } else {
+            const a=require('./'+filename)
+            a[functionName](paramToSaveData,eventEmitter)
+          }
+
+          console.log("The file was saved!");
         });
-
-        req.on('end', ()=>{
-
-            console.log("Client post data : " + postData);
-
-            // Parse the post data and get client sent username and password.
-            var postDataObject = JSON.parse(postData);
-            let filename=postDataObject.filename;
-            let code=postDataObject.code;
-            
-            vm.runInThisContext('(function(require,paramToSaveData,eventEmitter) {const PUPPETEER_OPTS='+JSON.stringify(PUPPETEER_OPTS)+'; '+code+''+postDataObject.functionName+'(paramToSaveData,eventEmitter)}); ')(require,paramToSaveData);
-            //vm.runInThisContext('model()')
-            resp.end('++'+JSON.stringify(postDataObject)+'++');
-            fs.writeFile(filename, code, function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-            
-                console.log("The file was saved!");
-            }); 
-        })
+      });
     }
+  })
+  .listen(8124);
 
-  }).listen(8124);
+setInterval(() => {
+  console.log(paramToSaveData);
+}, 10000);
 
-  setInterval(() => {console.log(paramToSaveData)},10000);
-
-  console.log('Server running at http://127.0.0.1:8124/');
+console.log("Server running at http://127.0.0.1:8124/");
